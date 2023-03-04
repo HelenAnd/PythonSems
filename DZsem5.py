81efda8da5db447ff93a890599822f3753f75b09
Helen <bernatovich-51@gmail.com>
2023-03-04 11:27:03 +0300

    Add task 2

--- DZsem5.py
+++ DZsem5.py
@@ -4,14 +4,14 @@
 # A = 3; B = 5 -> 243 (3⁵)
 #     A = 2; B = 3 -> 8 
 
-# def power(base, exp):
-#     if (exp == 1):
-#         return (base)
-#     if (exp != 1):
-#         return (base * power(base, exp - 1))
-# base = int(input("Введите число: "))
-# exp = int(input("Введите его степень: "))
-# print("Результат возведения в степень равен:", power(base, exp))
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введите число: "))
+exp = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", power(base, exp))
 
 # Задача 28: Напишите рекурсивную функцию sum(a, b), возвращающую сумму двух целых неотрицательных чисел. Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
 
@@ -19,13 +19,13 @@
 # 2 2
 #     4 
 
-# def recsum(a, b):
-#     return recsum(a+1, b-1) if b > 0 else recsum(a-1, b+1) if b < 0 else a
+def recsum(a, b):
+    return recsum(a+1, b-1) if b > 0 else recsum(a-1, b+1) if b < 0 else a
  
  
-# a = int(input("A = "))
-# b = int(input("B = "))
-# print(f"A + B = {recsum(a, b)}")
+a = int(input("A = "))
+b = int(input("B = "))
+print(f"A + B = {recsum(a, b)}")
 
 # Задача 1: Строка представляет собой арифметическое выражение из однозначных чисел и знаков + и -. Вычислите результат.
 # Пример
@@ -34,14 +34,14 @@
 # Вывод:
 # 4
 
-# expr = input()
-# res = int(expr[0])
-# for i in range(1, len(expr), 2):
-#     if expr[i] == '+':
-#         res += int(expr[i + 1])
-#     elif expr[i] == '-':
-#         res -= int(expr[i + 1])
-# print(res)
+expr = input()
+res = int(expr[0])
+for i in range(1, len(expr), 2):
+    if expr[i] == '+':
+        res += int(expr[i + 1])
+    elif expr[i] == '-':
+        res -= int(expr[i + 1])
+print(res)
 
 # Задача 2: Словом в данной задаче считается последовательность букв, ограниченная пробелами или началом или концом строки. Выведите все слова из строки в столбик. НЕЛЬЗЯ ПОЛЬЗОВАТЬСЯ МЕТОДАМИ СТРОК (split)
 # Формат ввода: вводится строка. Формат вывода: выведите слова из строки по одному.
