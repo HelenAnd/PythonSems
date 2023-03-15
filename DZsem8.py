46d22719b74beaddb8737e1e29d7e79004d0cea9
Helen <bernatovich-51@gmail.com>
2023-03-15 13:40:47 +0300

    Add task

---
+++ DZsem8.py
@@ -0,0 +1,15 @@
+# Напишите функцию read_last(lines, file), которая будет открывать определенный файл file и выводить на печать построчно последние строки в количестве lines 
+# (на всякий случай проверим, что задано положительное целое число). 
+# Протестируем функцию на файле «article.txt» со следующим содержимым:
+
+def read_last(lines, file):
+    if lines > 0:
+        with open(file, encoding='utf-8') as text:
+            file_lines = text.readlines()[-lines:]
+        for line in file_lines:
+            print(line.strip())
+        else:
+            print('Количество строк может быть только целым положительным')
+
+read_last(2, 'article.txt')
+
